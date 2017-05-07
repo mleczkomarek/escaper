d9921a01eb4e57d239b35ca3b63621f1db9663fc
wrymckp <garncarz27134@student.pwsztar.edu.pl>
2017-05-07 18:46:57 +0200

    oswietlenie

---
+++ Assets/Standard Assets/Skrypty/FlickerLight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLight : MonoBehaviour {
+
+    public GameObject Lights;
+    public float timer;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(FlickeringLight());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator FlickeringLight()
+    {
+        Lights.SetActive(true);
+        timer = Random.Range(0.1f, 1);
+        yield return new WaitForSeconds(timer);
+        Lights.SetActive(false);
+        timer = Random.Range(0.1f, 1);
+        yield return new WaitForSeconds(timer);
+        StartCoroutine(FlickeringLight());
+    }
+}
