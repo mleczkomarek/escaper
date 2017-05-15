8b0162b3b1807ec9f4d8d50def51cbc4bbedd0b9
Marek Mleczko <marekmleczko111@gmail.com>
2017-05-15 18:01:36 +0200

    zrobione menu

---
+++ Assets/Standard Assets/menu/Settings.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+
+    public InputField musicControl;
+
+	// Use this for initialization
+	void Start () {
+        musicControl.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        musicControl.text = (AudioListener.volume * 100).ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ValueChangeCheck() {
+        int value;
+        if (int.TryParse(musicControl.text, out value)) {
+
+            if (value < 0)
+            {
+                value = 0;
+                musicControl.text = "0";
+            }
+            if (value > 100)
+            {
+                value = 100;
+                musicControl.text = "100";
+            }
+
+
+            float audioVolume = (float)value / 100.0f;
+
+            AudioListener.volume = audioVolume;
+        }
+    }
+}
---
+++ Assets/Standard Assets/menu/menu.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menu : MonoBehaviour, IPointerClickHandler {
+
+    public Text Text;
+    public Text[] textToShow;
+    public Text[] textToHide;
+    public GameObject inputToHide = null;
+	// Use this for initialization
+	void Start () {
+        if (Text.text.ToLower() == "ustawienia")
+        {
+            foreach (Text item in textToShow)
+            {
+                item.enabled = false;
+            }
+            if (inputToHide != null)
+                inputToHide.SetActive(false);
+        }
+        if (Text.text.ToLower().StartsWith("fullscreen"))
+            Text.text = Screen.fullScreen ? "Fullscreen - TAK" : "Fullscreen - NIE";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (Text.text.ToLower() == "nowa gra")
+        {
+            SceneManager.LoadScene("scena");
+        }
+        else if (Text.text.ToLower() == "ustawienia")
+        {
+            foreach (Text item in textToShow)
+            {
+                item.enabled = true;
+            }
+            foreach (Text item in textToHide)
+            {
+                item.enabled = false;
+            }
+            inputToHide.SetActive(true);
+        }
+        else if (Text.text.ToLower().StartsWith("fullscreen"))
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+            Text.text = Screen.fullScreen ? "Fullscreen - TAK" : "Fullscreen - NIE";
+
+        }
+        else if (Text.text.ToLower() == "wróć") {
+            foreach (Text item in textToShow)
+            {
+                item.enabled = true;
+            }
+            foreach (Text item in textToHide)
+            {
+                item.enabled = false;
+            }
+            inputToHide.SetActive(false);
+        }
+        else if (Text.text.ToLower() == "wyjście")
+        {
+            Application.Quit();
+        }
+    }
+}
